a7c01a5e1a56886b7058ff6d6c8779d7ea17a810
Beatrice Guerra <beatrice.guerra@zextras.com>
2022-04-26 14:29:58 +0200

    fix: update search params on query clear
    refs: FILES-216

--- src/views/SearchView.tsx
+++ src/views/SearchView.tsx
@@ -4,13 +4,14 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 import type { QueryChip, SearchViewProps } from '@zextras/carbonio-shell-ui';
 import { intersectionBy } from 'lodash';
 import forEach from 'lodash/forEach';
 import map from 'lodash/map';
 import partition from 'lodash/partition';
+import size from 'lodash/size';
 import some from 'lodash/some';
 import { useTranslation } from 'react-i18next';
 
@@ -26,13 +27,6 @@ const SearchView: React.VFC<SearchViewProps> = ({ useQuery, ResultsHeader }) => 
 	const [query, updateQuery] = useQuery();
 	const [t] = useTranslation();
 
-	const updateQueryWrapper = useCallback<(args: QueryChip[]) => void>(
-		(args) => {
-			updateQuery(args);
-		},
-		[updateQuery]
-	);
-
 	useEffect(() => {
 		const [advanced, keywords] = partition<QueryChip, AdvancedSearchChip>(
 			query,
@@ -47,9 +41,15 @@ const SearchView: React.VFC<SearchViewProps> = ({ useQuery, ResultsHeader }) => 
 		});
 		// check if values in query are different from values in searchParamsVar. If so, update searchParamsVar in order
 		// to keep the searches synced.
-		if (
-			intersectionBy(keywords, searchParamsVar().keywords || [], 'value').length !==
-				keywords.length ||
+		const searchParamsVarKeywords = searchParamsVar().keywords || [];
+
+		if (size(updatedValue) === 0 && size(searchParamsVar()) > 0) {
+			// if query is updated from outside and is empty (no keywords and no advanced)
+			// then a "clear" has been executed, so clear also searchParamsVar
+			searchParamsVar({});
+		} else if (
+			keywords.length !== searchParamsVarKeywords.length ||
+			intersectionBy(keywords, searchParamsVarKeywords, 'value').length !== keywords.length ||
 			some(advanced, (value) => searchParamsVar()[value.varKey]?.value !== value.value)
 		) {
 			// FIXME: since we cannot set hidden chips in query, we need to manually set the hidden params in
@@ -66,7 +66,7 @@ const SearchView: React.VFC<SearchViewProps> = ({ useQuery, ResultsHeader }) => 
 	return (
 		<PreventDefaultDropContainer>
 			<ProvidersWrapper>
-				<UpdateQueryContext.Provider value={updateQueryWrapper}>
+				<UpdateQueryContext.Provider value={updateQuery as (args: QueryChip[]) => void}>
 					<CommonSearchView
 						resultsHeader={<ResultsHeader label={t('search.resultsFor', 'Results for:')} />}
 						listWidth="25%"
