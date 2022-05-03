21faeed59ec6846a966925a3548dd13dac04d2de
Cataldo <cm@zextras.com>
2022-05-03 16:22:51 +0200

    feat: allow send via mail
    refs: FILES-162 FILES-197 (#46)

---
+++ src/hooks/useSendViaMail.ts
@@ -0,0 +1,50 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { useCallback } from 'react';
+
+import { useTranslation } from 'react-i18next';
+
+import { useCreateSnackbar } from '../carbonio-files-ui-common/hooks/useCreateSnackbar';
+import { uploadToTargetModule } from '../carbonio-files-ui-common/utils/utils';
+import { getComposePrefillMessageFunction } from '../integrations/functions';
+import { captureException } from '../utils/utils';
+
+export function useSendViaMail(): {
+	sendViaMail: (nodeId: string) => void;
+} {
+	const createSnackbar = useCreateSnackbar();
+	const [t] = useTranslation();
+
+	const sendViaMail = useCallback(
+		(nodeId: string) => {
+			uploadToTargetModule({ nodeId, targetModule: 'MAILS' }).then(
+				(result) => {
+					const { integratedFunction, available } = getComposePrefillMessageFunction();
+					if (available) {
+						integratedFunction({ aid: [result.attachmentId] });
+					}
+				},
+				(reason) => {
+					captureException(new Error(`Failure on uploadToTargetModule`));
+					console.error(reason);
+					createSnackbar({
+						key: new Date().toLocaleString(),
+						type: 'warning',
+						label: t('errorCode.code', 'Something went wrong', { context: 'Generic' }),
+						replace: true,
+						hideButton: true
+					});
+				}
+			);
+		},
+		[createSnackbar, t]
+	);
+
+	return {
+		sendViaMail
+	};
+}
---
+++ src/integrations/functions.ts
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { getIntegratedFunction } from '@zextras/carbonio-shell-ui';
+
+export type ComposePrefillMessageType = (args: { aid: string[] }) => void;
+
+export function getComposePrefillMessageFunction(): {
+	integratedFunction: ComposePrefillMessageType;
+	available: boolean;
+} {
+	const [integratedFunction, available] = getIntegratedFunction('composePrefillMessage');
+
+	return {
+		integratedFunction: integratedFunction as ComposePrefillMessageType,
+		available
+	};
+}
