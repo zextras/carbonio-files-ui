f2e40441dbaeee4caf272bd3cbec0c9bed50ae83
Beatrice Guerra <beatrice.guerra@zextras.com>
2022-09-23 10:17:47 +0200

    test: update to jest 28
    refs: FILES-338 (#119)

--- src/mocks/fileTransformer.js
+++ src/mocks/fileTransformer.js
@@ -3,13 +3,13 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
-// @see https://jestjs.io/docs/webpack
 module.exports = {
-	process(src, filename, _config, _options) {
-		return `module.exports = ${JSON.stringify(path.basename(filename))};`;
+	process(sourceText, sourcePath, _options) {
+		return {
+			code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`
+		};
 	}
 };
