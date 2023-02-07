3b48a809fac7a657fc4fa4b0730f867a3e71ef01
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-02-07 14:19:55 +0100

    fix: improve return for upload client queries
    Make client queries return null instead of undefined so that
    when reading from the cache, apollo always treats data as loaded.
    Null implies data are loaded and not present, while undefined implies
    data are partial or not loaded.
    
    refs: FILES-531

--- src/carbonio-files-ui-common/apollo/typePolicies/fieldPolicies/getUploadItem.ts
+++ src/carbonio-files-ui-common/apollo/typePolicies/fieldPolicies/getUploadItem.ts
@@ -3,22 +3,27 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import { FieldFunctionOptions, FieldPolicy } from '@apollo/client';
+import type { FieldFunctionOptions, FieldPolicy } from '@apollo/client';
 
-import { UploadItem } from '../../../types/graphql/client-types';
+import type { UploadItem } from '../../../types/graphql/client-types';
+import type {
+	GetUploadItemQueryVariables,
+	QueryGetUploadItemArgs
+} from '../../../types/graphql/types';
 import { uploadVar } from '../../uploadVar';
 
 export const getUploadItemFieldPolicy: FieldPolicy<
 	unknown,
 	unknown,
-	UploadItem,
-	FieldFunctionOptions<{ id?: string }, { id?: string }>
+	UploadItem | null,
+	FieldFunctionOptions<Partial<QueryGetUploadItemArgs>, Partial<GetUploadItemQueryVariables>>
 > = {
 	read(_, options) {
 		const id = options.args?.id;
-		if (id) {
-			return uploadVar()[id];
-		}
-		return undefined;
+		// return null to indicate the entry is not present instead of undefined
+		// in order to tell the data has been searched, but it does not exist.
+		// undefined means that data is incomplete or not queried yet, but in this case
+		// if the data is not present it means it should not exist
+		return (id && uploadVar()[id]) || null;
 	}
 };
