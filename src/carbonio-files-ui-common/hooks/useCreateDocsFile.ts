017078ff0c5dfeead5199c5845701329a1c59986
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-07-04 16:28:05 +0200

    fix: save parent in cache with writeFragment when reading children
    cache.modify does not write the field if it is not already present
    
    refs: FILES-477

--- src/carbonio-files-ui-common/hooks/useCreateDocsFile.ts
+++ src/carbonio-files-ui-common/hooks/useCreateDocsFile.ts
@@ -4,26 +4,17 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 
-import { ApolloClient, FetchResult, NormalizedCacheObject, useReactiveVar } from '@apollo/client';
+import { ApolloClient, FetchResult, useApolloClient, useReactiveVar } from '@apollo/client';
 
 import { UpdateFolderContentType, useUpdateFolderContent } from './graphql/useUpdateFolderContent';
-import buildClient from '../apollo';
 import { nodeSortVar } from '../apollo/nodeSortVar';
 import { DOCS_ENDPOINT, CREATE_FILE_PATH, NODES_LOAD_LIMIT, SHARES_LOAD_LIMIT } from '../constants';
-import GET_CHILDREN from '../graphql/queries/getChildren.graphql';
 import GET_NODE from '../graphql/queries/getNode.graphql';
 import { CreateDocsFile, DocsType } from '../types/common';
-import {
-	Folder,
-	GetChildrenQuery,
-	GetChildrenQueryVariables,
-	GetNodeQuery,
-	GetNodeQueryVariables,
-	NodeSort
-} from '../types/graphql/types';
-import { scrollToNodeItem } from '../utils/utils';
+import { Folder, GetNodeQuery, GetNodeQueryVariables, NodeSort } from '../types/graphql/types';
+import { isFolder, scrollToNodeItem } from '../utils/utils';
 
 interface DocsFile {
 	name: string;
@@ -41,7 +32,7 @@ type UseCreateDocsFile = () => (
 const createFileCompleted = (
 	xhr: XMLHttpRequest,
 	file: DocsFile,
-	apolloClient: ApolloClient<NormalizedCacheObject>,
+	apolloClient: ApolloClient<object>,
 	nodeSort: NodeSort,
 	addNodeToFolder: UpdateFolderContentType['addNodeToFolder']
 ): Promise<FetchResult<CreateDocsFile>> => {
@@ -62,53 +53,39 @@ const createFileCompleted = (
 				})
 				.then((result) => {
 					if (result.data.getNode?.parent) {
-						const parentFolder = apolloClient.cache.readQuery<
-							GetChildrenQuery,
-							GetChildrenQueryVariables
-						>({
-							query: GET_CHILDREN,
-							variables: {
-								node_id: result.data.getNode.parent.id,
-								// load all cached children
-								children_limit: Number.MAX_SAFE_INTEGER,
-								sort: nodeSort
-							}
-						});
-						if (parentFolder?.getNode?.__typename === 'Folder') {
-							const parentNode = parentFolder.getNode;
-							const { isLast } = addNodeToFolder(parentNode, result.data.getNode);
+						if (isFolder(result.data.getNode.parent)) {
+							const { isLast } = addNodeToFolder(result.data.getNode.parent, result.data.getNode);
 							scrollToNodeItem(result.data.getNode.id, isLast);
 						}
 					}
 					return result;
+				})
+				.catch((error) => {
+					console.error(error);
+					throw error;
 				});
 		}
 		case 413: {
-			// eslint-disable-next-line prefer-promise-reject-errors
-			return Promise.reject({ message: xhr.statusText });
+			return Promise.reject(new Error(xhr.statusText));
 		}
 		// name already exists
 		case 500: {
-			// eslint-disable-next-line prefer-promise-reject-errors
-			return Promise.reject({ message: xhr.statusText });
+			return Promise.reject(new Error(xhr.statusText));
 		}
 		default: {
 			console.error('Unhandled status', xhr.status, xhr.statusText);
-			// eslint-disable-next-line prefer-promise-reject-errors
-			return Promise.reject({ message: xhr.statusText });
+			return Promise.reject(new Error(xhr.statusText));
 		}
 	}
 };
 
 export const useCreateDocsFile: UseCreateDocsFile = () => {
-	// TODO use useApolloClient when apollo provider will be moved up in tha app
-	// const apolloClient = useApolloClient();
-	const apolloClient = useMemo(() => buildClient(), []);
+	const apolloClient = useApolloClient();
 
 	const nodeSort = useReactiveVar(nodeSortVar);
 	const { addNodeToFolder } = useUpdateFolderContent(apolloClient);
 
-	const createDocsFile = useCallback(
+	return useCallback(
 		(parentFolder: Pick<Folder, '__typename' | 'id' | 'children'>, name: string, type: DocsType) =>
 			new Promise<FetchResult<CreateDocsFile>>((resolve, reject) => {
 				const file = { name, type, parentId: parentFolder.id };
@@ -137,6 +114,4 @@ export const useCreateDocsFile: UseCreateDocsFile = () => {
 			}),
 		[apolloClient, nodeSort, addNodeToFolder]
 	);
-
-	return createDocsFile;
 };
