f585fa9ad1dac883235ac2a6b5b28b833b53eb18
Rodley Orosa <rodley.orosa@zextras.com>
2023-07-05 18:07:58 +0200

    style: remove double licence
    refs: FILES-645

--- src/components/BadgeWrap.tsx
+++ src/components/BadgeWrap.tsx
@@ -3,11 +3,6 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-/*
- * SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
- *
- * SPDX-License-Identifier: AGPL-3.0-only
- */
 import React from 'react';
 
 import { Container, Icon } from '@zextras/carbonio-design-system';
