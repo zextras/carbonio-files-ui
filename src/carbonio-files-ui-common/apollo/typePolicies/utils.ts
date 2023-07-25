8d398afe984c0ce01038f023ae9a7554788e1ec8
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-07-25 15:31:03 +0200

    fix: check if child is null beforw writing data in cache
    refs: FILES-705

--- src/carbonio-files-ui-common/apollo/typePolicies/utils.ts
+++ src/carbonio-files-ui-common/apollo/typePolicies/utils.ts
@@ -8,10 +8,11 @@ import { FieldFunctionOptions, Reference } from '@apollo/client';
 import { keyBy } from 'lodash';
 
 import { NodesListCachedObject } from '../../types/apollo';
+import { Maybe } from '../../types/graphql/types';
 
 export function mergeNodesList(
 	existing: NodesListCachedObject | undefined,
-	incoming: Reference[],
+	incoming: Maybe<Reference>[],
 	{ readField, mergeObjects }: FieldFunctionOptions
 ): NodesListCachedObject {
 	const newOrdered = keyBy(existing?.ordered, (item) => readField<string>('id', item) as string);
@@ -22,27 +23,29 @@ export function mergeNodesList(
 	// add all incoming items
 	// if an item was already loaded in existing ordered data it will be merged with the incoming one
 	const unCachedOrdered: Reference[] = [];
-	incoming.forEach((item: Reference) => {
-		const id = readField<string>('id', item) as string;
-		// if item is stored in cache id is valued
-		if (id) {
-			// check if item is stored inside the ordered nodes
-			const cachedOrderedNode = newOrdered[id];
-			if (cachedOrderedNode) {
-				// if it is stored inside ordered nodes, merge existing data with incoming ones
-				newOrdered[id] = mergeObjects(cachedOrderedNode, item);
+	incoming.forEach((item) => {
+		if (item) {
+			const id = readField<string>('id', item);
+			// if item is stored in cache id is valued
+			if (id) {
+				// check if item is stored inside the ordered nodes
+				const cachedOrderedNode = newOrdered[id];
+				if (cachedOrderedNode) {
+					// if it is stored inside ordered nodes, merge existing data with incoming ones
+					newOrdered[id] = mergeObjects(cachedOrderedNode, item);
+				} else {
+					// otherwise, add incoming data to the list of the uncached ordered nodes
+					unCachedOrdered.push(item);
+				}
+				// unOrderItem that now is ordered
+				if (newUnOrdered[id] != null) {
+					// remove item from the unOrdered since it's now in the ordered list
+					delete newUnOrdered[id];
+				}
 			} else {
-				// otherwise, add incoming data to the list of the uncached ordered nodes
+				// item wasn't store in cache, so add it directly in the uncached ordered nodes
 				unCachedOrdered.push(item);
 			}
-			// unOrderItem that now is ordered
-			if (newUnOrdered[id] != null) {
-				// remove item from the unOrdered since it's now in the ordered list
-				delete newUnOrdered[id];
-			}
-		} else {
-			// item wasn't store in cache, so add it directly in the uncached ordered nodes
-			unCachedOrdered.push(item);
 		}
 	});
 
