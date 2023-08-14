92b107bf75cb560f227ddf55c8206dbf04214016
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-08-14 09:22:36 +0200

    build(dev-deps): update babel dependencies
    refs: FILES-685

--- babel.config.js
+++ babel.config.js
@@ -7,8 +7,6 @@
 module.exports = {
 	presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
 	plugins: [
-		'@babel/plugin-transform-runtime',
-		'@babel/plugin-proposal-class-properties',
 		'babel-plugin-styled-components',
 		[
 			'i18next-extract',
