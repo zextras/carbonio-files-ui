86ea42734e646d64d03fb2b88d3e2e5dca337fb4
Rodley Orosa <rodley.orosa@zextras.com>
2023-08-31 11:36:15 +0200

    refactor: remove reduntant code
    refs: FILES-708

--- src/carbonio-files-ui-common/utils/previewUtils.ts
+++ src/carbonio-files-ui-common/utils/previewUtils.ts
@@ -80,11 +80,6 @@ export const MIME_TYPE_PREVIEW_SUPPORT: Record<
 		thumbnail: true,
 		thumbnail_detail: true,
 		preview: true
-	},
-	'text/plain': {
-		thumbnail: false,
-		thumbnail_detail: false,
-		preview: false
 	}
 } as const;
 
