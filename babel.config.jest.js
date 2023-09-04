f3df747f8ec1ebd15aff241546e5213f1d2cd15c
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-09-04 16:32:48 +0200

    build(dev-deps): update babel config on jest

--- babel.config.jest.js
+++ babel.config.jest.js
@@ -5,6 +5,16 @@
  */
 
 module.exports = {
-	presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+	presets: [
+		[
+			'@babel/preset-env',
+			{
+				useBuiltIns: 'usage',
+				corejs: 3.32
+			}
+		],
+		'@babel/preset-react',
+		'@babel/preset-typescript'
+	],
 	plugins: ['babel-plugin-styled-components']
 };
