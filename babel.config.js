84029003b97057ce01d5a1ea6ec978324c3815e5
Beatrice Guerra <beatrice.guerra@zextras.com>
2024-04-12 15:35:33 +0200

    build(dev-deps): remove babel-plugin-i18next-extract
    From now on, new labels should be added directly in weblate,
    so there is no need to extract them from the code.
    
    Refs: FILES-777

--- babel.config.js
+++ babel.config.js
@@ -16,18 +16,5 @@ module.exports = {
 		'@babel/preset-react',
 		'@babel/preset-typescript'
 	],
-	plugins: [
-		'babel-plugin-styled-components',
-		[
-			'i18next-extract',
-			{
-				outputPath: 'translations/{{ns}}.json',
-				defaultContexts: [],
-				discardOldKeys: false,
-				defaultNS: 'en',
-				jsonSpace: 4,
-				compatibilityJSON: 'v4'
-			}
-		]
-	]
+	plugins: ['babel-plugin-styled-components']
 };
