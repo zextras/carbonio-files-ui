f32cacf09691ac944519817202e440c57107e423
Cataldo Mazzilli <cm@zextras.com>
2024-06-26 09:15:16 +0200

    refactor: replace main list with ListV2
    Refs: FILES-785 (#366)

--- src/carbonio-files-ui-common/views/components/ListContent.tsx
+++ src/carbonio-files-ui-common/views/components/ListContent.tsx
@@ -6,23 +6,22 @@
 
 import React, { useCallback, useMemo, useRef, useState } from 'react';
 
-import type { Action as DSAction } from '@zextras/carbonio-design-system';
+import { ListV2, type Action as DSAction, Row } from '@zextras/carbonio-design-system';
 import { forEach, map, filter, includes } from 'lodash';
 import styled from 'styled-components';
 
 import { Draggable } from './Draggable';
 import { NodeListItem } from './NodeListItem';
 import { NodeListItemWrapper } from './NodeListItemWrapper';
-import { ScrollContainer } from './ScrollContainer';
 import { useUserInfo } from '../../../hooks/useUserInfo';
 import { draggedItemsVar } from '../../apollo/dragAndDropVar';
-import { DRAG_TYPES } from '../../constants';
+import { DRAG_TYPES, LIST_ITEM_HEIGHT } from '../../constants';
 import { DeleteNodesType } from '../../hooks/graphql/mutations/useDeleteNodesMutation';
 import { Action, GetNodeParentType, NodeListItemType, PickIdNodeType } from '../../types/common';
 import { Node } from '../../types/graphql/types';
 import { DeepPick, OneOrMany } from '../../types/utils';
 import { ActionsFactoryCheckerMap, getPermittedActions } from '../../utils/ActionsFactory';
-import { isFile } from '../../utils/utils';
+import { cssCalcBuilder, isFile } from '../../utils/utils';
 
 const DragImageContainer = styled.div`
 	position: absolute;
@@ -54,7 +53,6 @@ interface ListContentProps {
 	manageShares?: (nodeId: string) => void;
 	compact?: boolean;
 	navigateTo?: (id: string, event?: React.SyntheticEvent | Event) => void;
-	loading?: boolean;
 	hasMore?: boolean;
 	loadMore?: () => void;
 	draggable?: boolean;
@@ -63,179 +61,185 @@ interface ListContentProps {
 	fillerWithActions?: React.JSX.Element;
 }
 
-export const ListContent = React.forwardRef<HTMLDivElement, ListContentProps>(
-	function ListContentFn(
-		{
-			nodes,
-			selectedMap = {},
-			selectId,
-			isSelectionModeActive,
-			exitSelectionMode,
-			toggleFlag,
-			renameNode,
-			markNodesForDeletion,
-			restore,
-			deletePermanently,
-			moveNodes,
-			copyNodes,
-			activeNodes,
-			setActiveNode,
-			compact,
-			navigateTo,
-			loading = false,
-			hasMore = false,
-			loadMore = (): void => undefined,
-			draggable = false,
-			customCheckers,
-			selectionContextualMenuActionsItems,
-			fillerWithActions,
-			manageShares
-		},
-		ref
-	) {
-		const dragImageRef = useRef<HTMLDivElement>(null);
+export const ListContent = ({
+	nodes,
+	selectedMap = {},
+	selectId,
+	isSelectionModeActive,
+	exitSelectionMode,
+	toggleFlag,
+	renameNode,
+	markNodesForDeletion,
+	restore,
+	deletePermanently,
+	moveNodes,
+	copyNodes,
+	activeNodes,
+	setActiveNode,
+	compact,
+	navigateTo,
+	hasMore = false,
+	loadMore = (): void => undefined,
+	draggable = false,
+	customCheckers,
+	selectionContextualMenuActionsItems,
+	fillerWithActions,
+	manageShares
+}: ListContentProps): React.JSX.Element => {
+	const dragImageRef = useRef<HTMLDivElement>(null);
 
-		const { me } = useUserInfo();
+	const { me } = useUserInfo();
 
-		const [dragImage, setDragImage] = useState<React.JSX.Element[]>([]);
+	const [dragImage, setDragImage] = useState<React.JSX.Element[]>([]);
 
-		const dragStartHandler = useCallback<
-			(node: NodeListItemType) => React.DragEventHandler<HTMLElement>
-		>(
-			(node) =>
-				(event): void => {
-					forEach(DRAG_TYPES, (dragType) => event.dataTransfer.clearData(dragType));
-					const nodesToDrag: NodeListItemType[] = [];
-					if (isSelectionModeActive) {
-						nodesToDrag.push(...filter(nodes, ({ id }) => !!selectedMap[id]));
-					} else {
-						nodesToDrag.push(node);
-					}
-					const draggedItemsTmp: React.JSX.Element[] = [];
-					const permittedActions = getPermittedActions(
-						nodesToDrag,
-						[Action.Move, Action.MoveToTrash],
-						me,
-						undefined,
-						undefined,
-						customCheckers
-					);
-					forEach(nodesToDrag, (nodeToDrag) => {
-						draggedItemsTmp.push(
-							<NodeListItem
-								key={`dragged-${nodeToDrag.id}`}
-								id={`dragged-${nodeToDrag.id}`}
-								name={nodeToDrag.name}
-								type={nodeToDrag.type}
-								extension={(isFile(nodeToDrag) && nodeToDrag.extension) || undefined}
-								mimeType={(isFile(nodeToDrag) && nodeToDrag.mime_type) || undefined}
-								updatedAt={nodeToDrag.updated_at}
-								owner={nodeToDrag.owner}
-								lastEditor={nodeToDrag.last_editor}
-								incomingShare={me !== nodeToDrag.owner?.id}
-								outgoingShare={
-									me === nodeToDrag.owner?.id && nodeToDrag.shares && nodeToDrag.shares.length > 0
-								}
-								size={isFile(nodeToDrag) ? nodeToDrag.size : undefined}
-								flagActive={nodeToDrag.flagged}
-							/>
-						);
-					});
-					setDragImage(draggedItemsTmp);
-					dragImageRef.current && event.dataTransfer.setDragImage(dragImageRef.current, 0, 0);
-					draggedItemsVar(nodesToDrag);
-					if (includes(permittedActions, Action.Move)) {
-						event.dataTransfer.setData(DRAG_TYPES.move, JSON.stringify(nodesToDrag));
-					}
-					if (includes(permittedActions, Action.MoveToTrash)) {
-						event.dataTransfer.setData(DRAG_TYPES.markForDeletion, JSON.stringify(nodesToDrag));
-					}
-				},
-			[customCheckers, isSelectionModeActive, me, nodes, selectedMap]
-		);
-
-		const dragEndHandler = useCallback(() => {
-			setDragImage([]);
-			draggedItemsVar(null);
-		}, []);
-
-		const items = useMemo(
-			() =>
-				map(nodes, (node) => (
-					<Draggable
-						draggable={draggable}
-						onDragStart={dragStartHandler(node)}
-						onDragEnd={dragEndHandler}
-						key={node.id}
-						effect="move"
-					>
-						<NodeListItemWrapper
-							node={node}
-							toggleFlag={toggleFlag}
-							markNodesForDeletion={markNodesForDeletion}
-							restore={restore}
-							deletePermanently={deletePermanently}
-							moveNodes={moveNodes}
-							copyNodes={copyNodes}
-							manageShares={manageShares}
-							isSelected={selectedMap && selectedMap[node.id]}
-							isSelectionModeActive={isSelectionModeActive}
-							selectId={selectId}
-							exitSelectionMode={exitSelectionMode}
-							renameNode={renameNode}
-							isActive={
-								activeNodes === node.id ||
-								(activeNodes instanceof Array && activeNodes.includes(node.id))
+	const dragStartHandler = useCallback<
+		(node: NodeListItemType) => React.DragEventHandler<HTMLElement>
+	>(
+		(node) =>
+			(event): void => {
+				forEach(DRAG_TYPES, (dragType) => event.dataTransfer.clearData(dragType));
+				const nodesToDrag: NodeListItemType[] = [];
+				if (isSelectionModeActive) {
+					nodesToDrag.push(...filter(nodes, ({ id }) => !!selectedMap[id]));
+				} else {
+					nodesToDrag.push(node);
+				}
+				const draggedItemsTmp: React.JSX.Element[] = [];
+				const permittedActions = getPermittedActions(
+					nodesToDrag,
+					[Action.Move, Action.MoveToTrash],
+					me,
+					undefined,
+					undefined,
+					customCheckers
+				);
+				forEach(nodesToDrag, (nodeToDrag) => {
+					draggedItemsTmp.push(
+						<NodeListItem
+							key={`dragged-${nodeToDrag.id}`}
+							id={`dragged-${nodeToDrag.id}`}
+							name={nodeToDrag.name}
+							type={nodeToDrag.type}
+							extension={(isFile(nodeToDrag) && nodeToDrag.extension) || undefined}
+							mimeType={(isFile(nodeToDrag) && nodeToDrag.mime_type) || undefined}
+							updatedAt={nodeToDrag.updated_at}
+							owner={nodeToDrag.owner}
+							lastEditor={nodeToDrag.last_editor}
+							incomingShare={me !== nodeToDrag.owner?.id}
+							outgoingShare={
+								me === nodeToDrag.owner?.id && nodeToDrag.shares && nodeToDrag.shares.length > 0
 							}
-							setActive={setActiveNode}
-							compact={compact}
-							navigateTo={navigateTo}
-							selectionContextualMenuActionsItems={
-								selectedMap && selectedMap[node.id]
-									? selectionContextualMenuActionsItems
-									: undefined
-							}
+							size={isFile(nodeToDrag) ? nodeToDrag.size : undefined}
+							flagActive={nodeToDrag.flagged}
 						/>
-					</Draggable>
-				)),
-			[
-				nodes,
-				draggable,
-				dragStartHandler,
-				dragEndHandler,
-				toggleFlag,
-				markNodesForDeletion,
-				restore,
-				deletePermanently,
-				moveNodes,
-				copyNodes,
-				manageShares,
-				selectedMap,
-				isSelectionModeActive,
-				selectId,
-				exitSelectionMode,
-				renameNode,
-				activeNodes,
-				setActiveNode,
-				compact,
-				navigateTo,
-				selectionContextualMenuActionsItems
-			]
-		);
+					);
+				});
+				setDragImage(draggedItemsTmp);
+				dragImageRef.current && event.dataTransfer.setDragImage(dragImageRef.current, 0, 0);
+				draggedItemsVar(nodesToDrag);
+				if (includes(permittedActions, Action.Move)) {
+					event.dataTransfer.setData(DRAG_TYPES.move, JSON.stringify(nodesToDrag));
+				}
+				if (includes(permittedActions, Action.MoveToTrash)) {
+					event.dataTransfer.setData(DRAG_TYPES.markForDeletion, JSON.stringify(nodesToDrag));
+				}
+			},
+		[customCheckers, isSelectionModeActive, me, nodes, selectedMap]
+	);
 
-		return (
-			<>
-				<ScrollContainer
-					loading={loading}
-					hasMore={hasMore}
-					loadMore={loadMore}
-					ref={ref}
-					fillerWithActions={fillerWithActions}
-				>
-					{items}
-				</ScrollContainer>
-				<DragImageContainer ref={dragImageRef}>{dragImage}</DragImageContainer>
-			</>
-		);
-	}
-);
+	const dragEndHandler = useCallback(() => {
+		setDragImage([]);
+		draggedItemsVar(null);
+	}, []);
+
+	const intersectionObserverInitOptions = useMemo(() => ({ threshold: 0.5 }), []);
+
+	const items = useMemo(() => {
+		const nodeElements = map(nodes, (node) => (
+			<Draggable
+				draggable={draggable}
+				onDragStart={dragStartHandler(node)}
+				onDragEnd={dragEndHandler}
+				key={node.id}
+				effect="move"
+			>
+				<NodeListItemWrapper
+					node={node}
+					toggleFlag={toggleFlag}
+					markNodesForDeletion={markNodesForDeletion}
+					restore={restore}
+					deletePermanently={deletePermanently}
+					moveNodes={moveNodes}
+					copyNodes={copyNodes}
+					manageShares={manageShares}
+					isSelected={selectedMap && selectedMap[node.id]}
+					isSelectionModeActive={isSelectionModeActive}
+					selectId={selectId}
+					exitSelectionMode={exitSelectionMode}
+					renameNode={renameNode}
+					isActive={
+						activeNodes === node.id ||
+						(activeNodes instanceof Array && activeNodes.includes(node.id))
+					}
+					setActive={setActiveNode}
+					compact={compact}
+					navigateTo={navigateTo}
+					selectionContextualMenuActionsItems={
+						selectedMap && selectedMap[node.id] ? selectionContextualMenuActionsItems : undefined
+					}
+				/>
+			</Draggable>
+		));
+
+		if (fillerWithActions && !hasMore) {
+			nodeElements.push(
+				React.cloneElement(fillerWithActions, {
+					key: 'fillerWithActions',
+					children: <Row height={cssCalcBuilder(LIST_ITEM_HEIGHT, ['/', 2])} />
+				})
+			);
+		}
+
+		return nodeElements;
+	}, [
+		nodes,
+		fillerWithActions,
+		hasMore,
+		draggable,
+		dragStartHandler,
+		dragEndHandler,
+		toggleFlag,
+		markNodesForDeletion,
+		restore,
+		deletePermanently,
+		moveNodes,
+		copyNodes,
+		manageShares,
+		selectedMap,
+		isSelectionModeActive,
+		selectId,
+		exitSelectionMode,
+		renameNode,
+		activeNodes,
+		setActiveNode,
+		compact,
+		navigateTo,
+		selectionContextualMenuActionsItems
+	]);
+
+	return (
+		<>
+			<ListV2
+				maxHeight={'100%'}
+				height={'auto'}
+				data-testid="main-list"
+				background={'gray6'}
+				onListBottom={hasMore ? loadMore : undefined}
+				intersectionObserverInitOptions={intersectionObserverInitOptions}
+			>
+				{items}
+			</ListV2>
+			<DragImageContainer ref={dragImageRef}>{dragImage}</DragImageContainer>
+		</>
+	);
+};
