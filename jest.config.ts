e6be4675fc6c07eba6546cd281ab1746d8ae3f98
Cataldo Mazzilli <cm@zextras.com>
2024-11-18 10:40:03 +0100

    build(deps): update zextras dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -197,11 +197,11 @@ const config: Config = {
 
 	// A map from regular expressions to paths to transformers
 	transform: {
-		'^.+\\.[t|j]sx?$': ['babel-jest', { configFile: './babel.config.jest.js' }],
+		'^.+\\.[t|j]sx?$': 'babel-jest',
 		'\\.(gql|graphql)$': '@graphql-tools/jest-transform',
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
 			'<rootDir>/src/mocks/fileTransformer.js',
-		'\\.mjs?$': ['babel-jest', { configFile: './babel.config.jest.js' }]
+		'\\.mjs?$': 'babel-jest'
 	},
 
 	// An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
