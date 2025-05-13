2778c30ceeef0f9ba30098ff8e14d7dfaba4a3d8
Cataldo Mazzilli <cm@zextras.com>
2025-05-13 17:15:57 +0200

    fix: update merge in getNotificationsFieldPolicy

--- src/carbonio-files-ui-common/apollo/typePolicies/fieldPolicies/getNotifications.ts
+++ src/carbonio-files-ui-common/apollo/typePolicies/fieldPolicies/getNotifications.ts
@@ -4,11 +4,25 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { FieldPolicy } from '@apollo/client';
+import { FieldFunctionOptions, FieldPolicy, Reference } from '@apollo/client';
 
-export const getNotificationsFieldPolicy: FieldPolicy = {
+import { GetNotificationsQueryVariables, NotificationPage } from '../../../types/graphql/types';
+
+type CachedNotificationPage = Omit<NotificationPage, 'notifications'> & {
+	notifications: Array<Reference>;
+};
+
+export const getNotificationsFieldPolicy: FieldPolicy<
+	CachedNotificationPage,
+	CachedNotificationPage,
+	CachedNotificationPage,
+	FieldFunctionOptions<Partial<GetNotificationsQueryVariables>>
+> = {
 	keyArgs: false,
-	merge(existing, incoming) {
+	merge(existing, incoming, { args }) {
+		if (!args || args.update_last_seen) {
+			return incoming;
+		}
 		return {
 			...incoming,
 			last_seen: existing?.last_seen ?? incoming?.last_seen,
