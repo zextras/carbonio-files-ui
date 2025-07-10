43592aac87af42745a4467953ed47c3230cefed9
Rodley <100950196+rodleyorosa@users.noreply.github.com>
2025-07-10 09:17:59 +0200

    feat: change Collaboration link section to include all types
    Refs: FILES-941 (#493)

--- src/carbonio-files-ui-common/constants/test.ts
+++ src/carbonio-files-ui-common/constants/test.ts
@@ -91,7 +91,10 @@ export const ICON_REGEXP = {
 	noNotificationsIcon: 'icon: BellOffOutline',
 	secondaryBarNotifications: 'icon: BellOutline',
 	chevronRightNotifications: 'icon: ChevronRight',
-	chevronLeftNotifications: 'icon: ChevronLeft'
+	chevronLeftNotifications: 'icon: ChevronLeft',
+	eyeCollaborationLinkIcon: 'icon: EyeOutline',
+	editCollaborationLinkIcon: 'icon: Edit2Outline',
+	shareCollaborationLinkIcon: 'icon: ShareOutline'
 } as const;
 
 export const SELECTORS = {
@@ -103,9 +106,11 @@ export const SELECTORS = {
 	nodeAvatar: 'file-icon-preview',
 	nodeDetails: 'node-details',
 	nodeSharing: 'node-sharing',
-	collaborationLinkContainer: 'collaboration-link-container',
+	collaborationLinkReadOnly: 'read-only-collaboration-link-container',
 	collaborationLinkReadShare: 'read-share-collaboration-link-container',
 	collaborationLinkWriteShare: 'read-write-share-collaboration-link-container',
+	collaborationLinkWrite: 'read-write-collaboration-link-container',
+	collaborationLinkSelectLabel: 'collaboration-link-select-label',
 	sharingTabCollaborators: 'node-sharing-collaborators',
 	exclusiveSelectionEditor: 'exclusive-selection-editor',
 	exclusiveSelectionViewer: 'exclusive-selection-viewer',
