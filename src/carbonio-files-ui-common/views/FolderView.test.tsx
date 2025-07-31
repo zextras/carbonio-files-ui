b49b37fa6ec8bf3f5bd29abc3cf93a01a64ab3c0
rodleyorosa <rodley.orosa@zextras.com>
2025-07-31 15:08:52 +0200

    test: remove beforeEach
    Refs: FILES-964

--- src/carbonio-files-ui-common/views/FolderView.test.tsx
+++ src/carbonio-files-ui-common/views/FolderView.test.tsx
@@ -16,7 +16,6 @@ import * as network from '../../network/network';
 import { viewModeVar } from '../apollo/viewModeVar';
 import { VIEW_MODE } from '../constants';
 import { DISPLAYER_EMPTY_MESSAGE, ICON_REGEXP, SELECTORS } from '../constants/test';
-import * as useDownloadNodes from '../hooks/useDownloadNodes';
 import {
 	populateFile,
 	populateFolder,
@@ -55,13 +54,6 @@ jest.mock<typeof import('./components/VirtualizedNodeListItem')>(
 	'./components/VirtualizedNodeListItem'
 );
 jest.mock<typeof import('./components/NodeHoverBar')>('./components/NodeHoverBar');
-
-beforeEach(() => {
-	jest.spyOn(useDownloadNodes, 'useDownloadNodes').mockReturnValue({
-		downloadNode: jest.fn(),
-		downloadMultipleNodes: jest.fn()
-	});
-});
 
 describe('Folder View', () => {
 	describe('Create Folder', () => {
