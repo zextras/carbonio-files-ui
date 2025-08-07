4684e25763bb6c58cd957689e8b612c1398d0242
Cataldo Mazzilli <cm@zextras.com>
2025-08-07 11:42:07 +0200

    refactor: update useDownloadNodes

--- src/carbonio-files-ui-common/hooks/useDownloadNodes.ts
+++ src/carbonio-files-ui-common/hooks/useDownloadNodes.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 
 import { useSnackbar } from '@zextras/carbonio-design-system';
 import { useTranslation } from 'react-i18next';
@@ -25,74 +25,75 @@ export const useDownloadNodes = (): {
 	const [t] = useTranslation();
 	const configs = useGetConfigsQuery();
 
-	const limit = useMemo(() => Number(configs[CONFIGS.MAX_DOWNLOAD_SIZE]), [configs]);
+	const successSnackbarCallback = useCallback(() => {
+		createSnackbar({
+			key: new Date().toLocaleString(),
+			severity: 'info',
+			label: t('snackbar.download.start', 'Your download will start soon'),
+			replace: true,
+			hideButton: true
+		});
+	}, [createSnackbar, t]);
+
+	const errorSnackbarCallback = useCallback(() => {
+		const maxDownloadSize = configs[CONFIGS.MAX_DOWNLOAD_SIZE];
+		if (maxDownloadSize != null) {
+			createSnackbar({
+				key: new Date().toLocaleString(),
+				label: t(
+					'snackbar.download.error',
+					'Download size exceeds the {{limit}} limit. Please reduce items to download',
+					{
+						replace: {
+							limit: humanFileSizeFromMB(Number(maxDownloadSize), t)
+						}
+					}
+				),
+				severity: 'warning',
+				replace: true,
+				disableAutoHide: true
+			});
+		}
+	}, [configs, createSnackbar, t]);
+
+	const genericErrorSnackbar = useCallback(() => {
+		createSnackbar({
+			key: new Date().toLocaleString(),
+			severity: 'warning',
+			label: t('errorCode.code', 'Something went wrong', { context: 'Generic' }),
+			replace: true,
+			hideButton: true
+		});
+	}, [createSnackbar, t]);
 
 	const downloadNode = useCallback(
 		(id: string, version?: number) => {
 			downloadNodeFn(id, version).then((response) => {
 				if (response.ok) {
-					createSnackbar({
-						key: new Date().toLocaleString(),
-						severity: 'info',
-						label: t('snackbar.download.start', 'Your download will start soon'),
-						replace: true,
-						hideButton: true
-					});
-				}
-				if (response.status === HTTP_STATUS_CODE.fileSizeExceeded) {
-					createSnackbar({
-						key: new Date().toLocaleString(),
-						label: t(
-							'snackbar.download.error',
-							'Download size exceeds the {{limit}} limit. Please reduce items to download',
-							{
-								replace: {
-									limit: humanFileSizeFromMB(limit, t)
-								}
-							}
-						),
-						severity: 'warning',
-						replace: true,
-						disableAutoHide: true
-					});
+					successSnackbarCallback();
+				} else if (response.status === HTTP_STATUS_CODE.fileSizeExceeded) {
+					errorSnackbarCallback();
+				} else {
+					genericErrorSnackbar();
 				}
 			});
 		},
-		[createSnackbar, limit, t]
+		[errorSnackbarCallback, genericErrorSnackbar, successSnackbarCallback]
 	);
 
 	const downloadMultipleNodes = useCallback(
 		(nodeIds: string[]) => {
 			downloadMultipleNodesFn(nodeIds).then((response) => {
 				if (response.ok) {
-					createSnackbar({
-						key: new Date().toLocaleString(),
-						severity: 'info',
-						label: t('snackbar.download.start', 'Your download will start soon'),
-						replace: true,
-						hideButton: true
-					});
-				}
-				if (response.status === HTTP_STATUS_CODE.fileSizeExceeded) {
-					createSnackbar({
-						key: new Date().toLocaleString(),
-						label: t(
-							'snackbar.download.error',
-							'Download size exceeds the {{limit}} limit. Please reduce items to download',
-							{
-								replace: {
-									limit: humanFileSizeFromMB(limit, t)
-								}
-							}
-						),
-						severity: 'warning',
-						replace: true,
-						disableAutoHide: true
-					});
+					successSnackbarCallback();
+				} else if (response.status === HTTP_STATUS_CODE.fileSizeExceeded) {
+					errorSnackbarCallback();
+				} else {
+					genericErrorSnackbar();
 				}
 			});
 		},
-		[createSnackbar, limit, t]
+		[errorSnackbarCallback, genericErrorSnackbar, successSnackbarCallback]
 	);
 
 	return { downloadNode, downloadMultipleNodes };
